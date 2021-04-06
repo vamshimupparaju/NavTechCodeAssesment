23ffebb9d9ae2910c8e8ba10b1f0d6aca2675764
vamshimupparaju <82034829+vamshimupparaju@users.noreply.github.com>
2021-04-06 18:27:36 +0530

    Add files via upload

---
+++ NavTechAssessmentAPIModel/CustomizedCustomer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavTechAssessmentAPIModel
+{
+    public class CustomizedCustomer
+    {
+      
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }        
+        public string LastName { get; set; }
+        public string MobileNo { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string PinCode { get; set; }
+        public int CreatedBy { get; set; }
+    }
+}
---
+++ NavTechAssessmentAPIModel/CustomizedOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavTechAssessmentAPIModel
+{
+    public class CustomizedOrder
+    {
+        public int OrderId { get; set; }
+        public int ItemId { get; set; }
+        public int CustomerId { get; set; }
+        public int Quantity { get; set; }
+        public int CreatedBy { get; set; }
+    }
+}
